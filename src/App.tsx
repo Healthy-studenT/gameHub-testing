1d3518c6f5f2e53922d04cb922676028fddcfcf3
DESKTOP-Q8H1C8O\Aldrine <aldrine.quijano@irely.com>
2024-05-20 17:36:12 +0800

    Build color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (<Grid templateAreas={{
     base: `"nav" "main"`,
     lg: `"nav nav" "aside main"`
     }}>
-    <GridItem area="nav" bg="coral">Nav</GridItem>
+    <GridItem area="nav"><NavBar /></GridItem>
     <Show above="lg">
-      <GridItem area="aside" bg="gold">Aside</GridItem>
+      <GridItem area="aside">Aside</GridItem>
     </Show>
-    <GridItem area="main" bg="dodgerblue">Main</GridItem>
+    <GridItem area="main">Main</GridItem>
     </Grid>)
 }
 
